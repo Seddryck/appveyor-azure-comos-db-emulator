65d78bacccbfac57241c58bf737789757ec8958e
Cédric L. Charlier <cedric.charlier@yucom.be>
2018-01-19 15:40:11 +0100

    o

--- wait-connect-cosmosdb.ps1
+++ wait-connect-cosmosdb.ps1
@@ -2,24 +2,22 @@ $attempt = 0
 $max = 3
 $client = New-Object System.Net.Sockets.TcpClient([System.Net.Sockets.AddressFamily]::InterNetwork)
 
-while(!$client.Connected -and $attempt -lt $max) {
-
+do {
+	$attempt++
 	try {    
 		$client.Connect("127.0.0.1", 8081)
 		write-host "CosmosDB started"
-		$attempt++
 	}
-
+	
 	catch {
+		$client.Close()
 		if($attempt -eq $max) {
 			write-host "CosmosDB was not started"
-			$client.Close()
-			return
+		} else {
+			[int]$sleepTime = 5*$attempt
+			write-host "CosmosDB is not started. Retry after $sleepTime seconds..."
+			sleep $sleepTime;
 		}
 	}
+}while(!$client.Connected -and $attempt -lt $max)
 
-	[int]$sleepTime = 5*$attempt
-	write-host "CosmosDB is not started. Retry after $sleepTime seconds..."
-	sleep $sleepTime;
-	$client.Close()
-}
