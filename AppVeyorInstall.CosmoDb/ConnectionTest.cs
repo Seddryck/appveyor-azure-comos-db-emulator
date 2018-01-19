9cdbecb07e3d0c1e247aefb4f2fdacd4b2620fcf
Cédric L. Charlier <cedric.charlier@yucom.be>
2018-01-19 13:05:29 +0100

    Add application

---
+++ AppVeyorInstall.CosmoDb/ConnectionTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Microsoft.Azure.Documents;
+using System;
+using Microsoft.Azure.Documents.Client;
+using System.Net;
+
+namespace AppVeyor_CosmosDB
+{
+    public class ConnectionTest
+    {
+        private const string EndpointUrl = "https://localhost:8081";
+        private const string AuthKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+        [Test]
+        public void CheckConnection()
+        {
+            var client = new DocumentClient(new Uri(EndpointUrl), AuthKey);
+            var db = client.CreateDatabaseIfNotExistsAsync(new Database { Id = "FamilyDB" }).Result;
+            var checkdb = client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri("FamilyDB")).Result;
+            Assert.That(checkdb.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+
+        [Test]
+        public void CheckNotExistingDatabase()
+        {
+            var client = new DocumentClient(new Uri(EndpointUrl), AuthKey);
+            var ex = Assert.ThrowsAsync<DocumentClientException>(async () => await client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri("NotExistingDB")));
+            Assert.That(ex.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+
+        }
+    }
+}
